19b8da0d27a409dd465b616b256df8ab2045f86f
ymntt <ymntt@users.noreply.github.com>
2014-08-01 13:49:20 +0900

    Update showa.rb
    平成　西暦に対応

--- showa.rb
+++ showa.rb
@@ -5,8 +5,10 @@ print "Content-type: text/html; charset=UTF-8'"
 print "\r\n\r\n"
 
 print "<!DOCTYPE html>\n<html>\n<head lang=\"ja\">\n<meta charset=\"UTF-8\">\n<title>今年は昭和何年？</title>\n</head>\n<body>\n"
-print "今年は昭和"
+print "今年は<br>昭和"
 d = Date.today
-print(d.strftime("%Y"))
-print "年です"
+print(d.year - 1925)
+print "年です<br>"
+printf"平成#{d.year - 1988}年です<br>"
+print "西暦#{d.year}年です"
 print "<\/body>"
